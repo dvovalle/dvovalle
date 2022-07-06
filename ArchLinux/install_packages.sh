63a38cd7070c6873f0a7858d51bd0323609feb71
Danilo Valle de Oliveira <daniloka@kalunga.com.br>
2022-07-05 22:18:37 -0300

    A

--- ArchLinux/install_packages.sh
+++ ArchLinux/install_packages.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-systemctl enable NetworkManager
-systemctl restart NetworkManager
-
-loadkeys br-abnt2
-export LANG=en_US.UTF-8
-timedatectl set-ntp true
-hwclock --systohc
 
 pacman -Syyuu
-
-pacman -S terminus-font dosfstools os-prober mtools network-manager-applet networkmanager wpa_supplicant wireless_tools dialog sudo vim curl wget base base-devel linux-header linux-firmware nvidia nvidia-utils nvidia-libgl mesa nvidia-settings vulkan-icd-loader bluez bluez-utils xdg-utils xdg-user-dirs alsa-utils pulseaudio pulseaudio-bluetooth curl wget git vim
+pacman -S linux-header linux-firmware terminus-font dosfstools os-prober mtools network-manager-applet networkmanager wpa_supplicant wireless_tools dialog sudo vim base base-devel xorg-server xorg-xinit xorg-apps dmenu nvidia nvidia-utils nvidia-libgl mesa nvidia-settings vulkan-icd-loader bluez bluez-utils xdg-utils xdg-user-dirs alsa-utils  pulseaudio pulseaudio-bluetooth curl wget git vim i3-wm i3status i3blocks i3lock xfce4-terminal rofi fzf arandr xdotool wmctrl pcmanfm ranger thunar ttf-dejavu ttf-liberation noto-fonts firefox nitrogen picom lxappearance material-gtk-theme papirus-icon-theme archlinux-wallpaper openconnect oath-toolkit
