fbf5ff7534ac86642c18896daef8f8161a58f9a3
Danilo Valle de Oliveira <daniloka@kalunga.com.br>
2022-07-06 13:09:57 -0300

    I3

--- ArchLinux/config-i3/.config/Code - OSS/User/History/1c559820/GZpp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# https://www.systutorials.com/docs/linux/man/1-rdesktop/
-
-rdesktop -u "danilo ka" -d kalunga.com.br -p "kalunga07" -k br-abnt2 -g 1920x1080 -f -t -m -D -P -z -x l -r sound:off 172.16.201.23
