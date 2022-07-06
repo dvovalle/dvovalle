f11a06f514a7184a3c8bafee6cc4c2a031601ea2
Danilo Valle de Oliveira <daniloka@kalunga.com.br>
2022-07-06 13:07:30 -0300

    Config i3

---
+++ ArchLinux/config-i3/.config/Code - OSS/User/History/1c559820/t2cv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# https://www.systutorials.com/docs/linux/man/1-rdesktop/
+# rdesktop -u "danilo ka" -d kalunga.com.br -p "kalunga07" -k br-abnt2 -g 1920x1080 -f -t -m -D -P -z -x l -r sound:off 172.16.201.23
+
+
+# https://raw.githubusercontent.com/awakecoding/FreeRDP-Manuals/master/User/FreeRDP-User-Manual.pdf
+
+xfreerdp /v:172.16.201.23 /u:"danilo ka" /p:"kalunga07" /d:kalunga.com.br /w:1920 /h:1080 /t:MADESENVNT29 
+
