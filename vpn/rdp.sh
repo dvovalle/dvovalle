cfa463014c5fe967bb269645a9fd621c5060826a
Danilo Valle <dvovalle@gmail.com>
2023-10-26 00:17:49 -0300

    Config VPN

--- vpn/rdp.sh
+++ vpn/rdp.sh
@@ -2,6 +2,6 @@
 
 # read -p "Informe o numero da máquina: " MAQUINA
 
-xfreerdp /v:172.20.80.85 /u:"daniloka" /p:"kalunga10" /d:kalunga.com.br /w:1920 /h:1080 /t:MADESENVNT29 /f +fonts /floatbar
+xfreerdp /v:172.20.80.85 /u:"daniloka" /p:"kalunga11" /d:kalunga.com.br /w:1920 /h:1080 /t:MADESENVNT29 /f +fonts /floatbar
 
 exit 0
