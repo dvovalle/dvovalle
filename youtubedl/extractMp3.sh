9fca10bbe728112e171213d7c4769478723dfdaa
Danilo Valle de Oliveira <daniloka@kalunga.com.br>
2024-03-21 23:13:50 -0300

    ajustes

---
+++ youtubedl/extractMp3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo 'Iniciando'
+# https://github.com/yt-dlp/yt-dlp
+
+# yt-dlp --extract-audio --audio-format mp3 $1
+yt-dlp -f 'ba' -x --audio-format mp3 $1  -o '/home/danilo/Music/download/%(title)s.%(ext)s'
+
+ls -l '/home/danilo/Music/download'
---
+++ youtubedl/pip_upgrade.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+export TESTING=False
+export PROPAGATE_EXCEPTIONS=true
+export SECRET_KEY='FF2B76B2-6C43-44CB-95BA-7979D49F7123'
+
+export TZ=America/Sao_Paulo
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_COLLATE=C
+export LC_TIME=en_US.UTF-8
+export PYTHONDONTWRITEBYTECODE=1
+export PYTHONUNBUFFERED=1
+
+rm -rf requirements_dev.txt
+
+pip install --upgrade pip
+
+for i in $(pip list -o | awk 'NR > 2 {print $1}')
+do
+    pip install -U $i
+done
+
+pip freeze -r requirements.txt -l > requirements_dev.txt
